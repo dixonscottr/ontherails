ed586b889f28d7cfffd18c07abd0c6ee3a4c4b14
Jason Perlman <email": null>
2016-11-23 11:24:35 -0500

    Add method to find arrival/departure times

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -5,24 +5,26 @@ class StationsController < ApplicationController
     data=Net::HTTP.get(URI.parse("http://datamine.mta.info/mta_esi.php?key=5a44f5292fb0076e8f17017858ce3c58"))
     feed = Transit_realtime::FeedMessage.decode(data)
 
-    feed.entity.each_with_index do |item,i|
-      p "trip_update #{item.trip_update.stop_time_update[-1].stop_id} #{i}" if item.trip_update
-      p "vehicle #{item.vehicle.stop_id} #{i}" if item.vehicle
-    end
+    # feed.entity.each_with_index do |item,i|
+    #   p "trip_update #{item.trip_update.stop_time_update[-1].stop_id} #{i}" if item.trip_update
+    #   p "vehicle #{item.vehicle.stop_id} #{i}" if item.vehicle
+    # end
 
-    x = feed.entity.select do |item|
+    entities_with_trip_update = feed.entity.select do |item|
       item if item.trip_update
     end
-    departures = x.select do |item|
-      item if (item.trip_update.stop_time_update[-1].departure == nil)
-    end
-    arrivals = x.select do |item|
-      item if (item.trip_update.stop_time_update[-1].arrival)
-    end
+    # departures = x.select do |item|
+    #   item if (item.trip_update.stop_time_update[-1].departure == nil)
+    # end
+    # arrivals = x.select do |item|
+    #   item if (item.trip_update.stop_time_update[-1].arrival)
+    # end
+    #
+    # y = feed.entity.select do |item|
+    #   item if item.vehicle
+    # end
 
-    y = feed.entity.select do |item|
-      item if item.vehicle
-    end
+    hashed_entities = find_arrival_departure_times(entities_with_trip_update)
 
 
     byebug
@@ -35,14 +37,29 @@ class StationsController < ApplicationController
 
   end
 
+
   private
 
-    def find_arrival_departure_times(entities_with_trip_update)
-      entities_with_trip_update.each do |entity|
-        entitye
+  def find_arrival_departure_times(entities_with_trip_update)
+    entities_hash = {}
+    entities_with_trip_update.each_with_index do |entity, idx|
+      entities[idx] = {}
+      entities[idx][:route_id] = entity.trip_update.trip.route_id
+      entities[idx][:trip_id] = entity.trip_update.trip.trip_id
+      entities[idx][:stop_time] = {}
+      entity.trip_update.stop_time_update.each_with_index do |stop_time, idx_b|
+        entities[idx][:stop_time][idx_b] = {}
+        if stop_time.arrival
+          entities[idx][:stop_time][idx_b][:arrival] = Time.at(stop_time.arrival.time)
+        end
+        if stop_time.departure
+          entities[idx][:stop_time][idx_b][:departure] = Time.at(stop_time.departure.time)
+        end
+        entities[idx][:stop_time][idx_b][:stop_id] = stop_time.stop_id
       end
-
     end
+    entities_hash
+  end
 
   # def parseData()
   #    @trains["entity"].each do |train|
