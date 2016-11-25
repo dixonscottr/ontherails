de1010ebf4334ae9ea7afb03ccd041f5d32e24ab
jpperlm <jasonpperlman@gmail.com>
2016-11-24 23:36:56 -0500

    Almost got this working fully. Have to fix the station code a lil bit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def convertStations(stations)
+    stations.map do |station|
+      {stationPos: {lat: station.latitude.to_f, lng: station.longitude.to_f}, stop_id: station.stop_id}
+    end.to_json.html_safe
+  end
+
+  def convertRoutes(routes)
+    routes.map do |route|
+      route.map do |station|
+        {stationPos: {lat: station.latitude.to_f, lng: station.longitude.to_f}, stop_id: station.stop_id}
+      end
+    end.to_json.html_safe
+  end
 end
