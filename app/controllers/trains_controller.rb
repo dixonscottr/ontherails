62a4cce94465712ccdfddac4adcf68ddd05f6af8
Scott Dixon <email": null>
2016-11-25 21:05:42 -0500

    Refactor update trains method

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -1,59 +1,17 @@
 class TrainsController < ApplicationController
 
   def update_trains
-    data=Net::HTTP.get(URI.parse("http://datamine.mta.info/mta_esi.php?key=#{ENV['mta_key']}"))
+    data = Net::HTTP.get(URI.parse("http://datamine.mta.info/mta_esi.php?key=#{ENV['mta_key']}"))
     feed = Transit_realtime::FeedMessage.decode(data)
 
-    trip_ids_with_vehicle = feed.entity.map do |entity|
-      if entity.vehicle
-        entity.vehicle.trip.trip_id
-      end
-    end
+    trip_ids_with_vehicle = get_trip_id_array(feed)
+    entities_with_vehicles = find_entities_with_vehicles(feed)
+    entities_with_trip_update = find_entities_with_trip_update(feed)
+    entities_on_tracks = find_entities_on_tracks(entities_with_trip_update, trip_ids_with_vehicle)
+    hashed_train_data = find_arrival_departure_times(entities_on_tracks)
+    add_timestamp(hashed_train_data, feed)
+    render json: hashed_train_data
 
-
-    entities_with_trip_update = feed.entity.select do |entity|
-      entity.trip_update
-    end
-
-    entities_on_the_track = entities_with_trip_update.select do |entity|
-      trip_ids_with_vehicle.compact.include?(entity.trip_update.trip.trip_id)
-    end
-
-
-    #
-    # entities = []
-    # feed.entity.each_with_index do |entity, idx|
-    #   if entity.vehicle
-    #     entities.push({})
-    #     entities[-1][:trip_id] = entity.vehicle.trip.trip_id
-    #     entities[-1][:route_id] = entity.vehicle.trip.route_id
-    #     entities[-1][:timestamp] = Time.at(entity.vehicle.timestamp)
-    #     entities[-1][:stop_id] = entity.vehicle.stop_id
-    #     associated_entity = feed.entity.find do |entity|
-    #       (entity.trip_update.trip.trip_id == entities[-1][:trip_id]) if entity.trip_update
-    #     end
-    #     # debugger
-    #     entities[-1][:time_of_arrival] = Time.at(associated_entity.trip_update.stop_time_update.first.arrival.time) if associated_entity.trip_update.stop_time_update.first.arrival
-    #   end
-    #
-    # end
-
-
-
-    hashed_entities = find_arrival_departure_times(entities_on_the_track)
-    # debugger
-    # latest_stops = find_latest_stop(entities_on_the_track)
-    # @stations = hashed_entities.each_with_index do |entity|
-    #   stop_to_find = entity[1][:stop_time][0][:stop_id]
-    #   Station.where(stop_id: entity[:stop_time][0][:stop_id])
-    # end
-    hashed_entities[:time_updated] = feed.header.timestamp
-
-
-    render json: hashed_entities
-
-    # debugger
-    # @updated_trips = entities
   end
 
   def previous_station
@@ -75,29 +33,53 @@ class TrainsController < ApplicationController
 
   private
 
-    def find_arrival_departure_times(entities_with_trip_update)
-      entities = {}
-      entities_with_trip_update.each_with_index do |entity, idx|
-        entities[idx] = {}
-        entities[idx][:route_id] = entity.trip_update.trip.route_id
-        entities[idx][:trip_id] = entity.trip_update.trip.trip_id
-        entities[idx][:stop_time] = {}
-        entity.trip_update.stop_time_update.each_with_index do |stop_time, idx_b|
-          entities[idx][:stop_time][idx_b] = {}
-          if stop_time.arrival
-            entities[idx][:stop_time][idx_b][:arrival] = stop_time.arrival.time
-          end
-          if stop_time.departure
-            entities[idx][:stop_time][idx_b][:departure] = stop_time.departure.time
-          end
-          entities[idx][:stop_time][idx_b][:stop_id] = stop_time.stop_id
-        end
+    def find_entities_with_vehicles(feed)
+      feed.entity.select do |entity|
+        entity.vehicle
       end
-      entities
     end
 
-    def filter_for_trips_with_vehicles
+    def add_timestamp(hashed_entities, feed)
+      hashed_entities[:time_updated] = feed.header.timestamp
+    end
 
+    def get_trip_id_array(feed)
+      feed.entity.map do |entity|
+        entity.vehicle.trip.trip_id if entity.vehicle
+      end
+    end
+
+    def find_entities_on_tracks(entities, trip_id_array)
+      entities.select do |entity|
+        trip_id_array.compact.include?(entity.trip_update.trip.trip_id)
+      end
+    end
+
+    def find_entities_with_trip_update(feed)
+      feed.entity.select do |entity|
+        entity.trip_update
+      end
+    end
+
+    def find_arrival_departure_times(entities)
+      entities_hash = {}
+      entities.each_with_index do |entity, idx|
+        entities_hash[idx] = {}
+        entities_hash[idx][:route_id] = entity.trip_update.trip.route_id
+        entities_hash[idx][:trip_id] = entity.trip_update.trip.trip_id
+        entities_hash[idx][:stop_time] = {}
+        entity.trip_update.stop_time_update.each_with_index do |stop_time, idx_b|
+          entities_hash[idx][:stop_time][idx_b] = {}
+          if stop_time.arrival
+            entities_hash[idx][:stop_time][idx_b][:arrival] = stop_time.arrival.time
+          end
+          if stop_time.departure
+            entities_hash[idx][:stop_time][idx_b][:departure] = stop_time.departure.time
+          end
+          entities_hash[idx][:stop_time][idx_b][:stop_id] = stop_time.stop_id
+        end
+      end
+      entities_hash
     end
 
     def find_latest_stop(entities_with_trip_update)
