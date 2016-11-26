c38a3ed609e4aa120c04d289a179f9c296ee0ca5
Scott Dixon <email": null>
2016-11-25 21:20:14 -0500

    Refactor find previous station and move to Lines controller

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -1,2 +1,27 @@
 class LinesController < ApplicationController
+
+    def find_previous_station
+      line = params[:line]
+      direction = params[:direction]
+      stop_id = params[:station]
+      timestamp = params[:time].to_i
+      lines_to_search = Line.where(line_identifier: line)
+      line_found = find_current_running_line(lines_to_search, timestamp)
+      prev_stn = line_found.find_previous_station(stop_id, direction)
+      previous_station_info = {}
+      if prev_stn
+        previous_station_info[:prev_station] = prev_stn.stop_id
+      end
+      render json: previous_station_info
+    end
+
+    private
+
+    def find_current_running_line(lines_array, current_time)
+      found_lines = lines_array.select do |line|
+        ((Time.parse(line.time_start)).to_i < current_time) && ((Time.parse(line.time_stop)).to_i > current_time)
+      end
+      found_lines.first
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'stations#index'
-  resources :stations
+  resources :stations, only: [:index]
   get '/update_trains' => 'trains#update_trains'
-  post '/previous_station' => 'trains#previous_station'
-
+  post '/find_previous_station' => 'lines#find_previous_station'
 end
