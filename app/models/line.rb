41831955a65e679485faad103f3f74f3f1c65cbd
Jason Perlman <email": null>
2016-11-28 10:45:22 -0500

    Trying code

--- app/models/line.rb
+++ app/models/line.rb
@@ -13,22 +13,29 @@ class Line < ApplicationRecord
 
 
   def find_previous_station(stop_id, direction)
-    station = Station.find_by(stop_id: stop_id)
-    ordered_stations = self.stationlines.order('created_at').map do |station|
-      Station.find(station.station_id)
-    end
+    current_station_line = Stationline.find_by(line: self, station: Station.find_by(stop_id: stop_id))
     if direction == 'N'
-        if (ordered_stations.index(station))
-          ordered_stations[ordered_stations.index(station) + 1]
-        else
-          false
-        end
+      Stationline.find(current_station_line.id + 1).station
     else
-      if (ordered_stations.index(station))
-        ordered_stations[ordered_stations.index(station) - 1]
-      else
-        false
-      end
+      Stationline.find(current_station_line.id - 1).station
     end
   end
+
+    # station = Station.find_by(stop_id: stop_id)
+    # ordered_stations = self.stationlines.order('created_at').map do |station|
+    #   Station.find(station.station_id)
+    # end
+    # if direction == 'N'
+    #     if (ordered_stations.index(station))
+    #       ordered_stations[ordered_stations.index(station) + 1]
+    #     else
+    #       false
+    #     end
+    # else
+    #   if (ordered_stations.index(station))
+    #     ordered_stations[ordered_stations.index(station) - 1]
+    #   else
+    #     false
+    #   end
+    # end
 end
