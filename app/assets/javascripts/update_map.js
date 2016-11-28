cff64ed8971838fd2906f503a47a9808db23bfae
Jacob Williams <email": null>
2016-11-28 15:59:03 -0500

    Refactor: setTrainIconSize moved into js components directory

--- app/assets/javascripts/update_map.js
+++ app/assets/javascripts/update_map.js
@@ -374,18 +374,7 @@ function updateTrainPosition(responseJSON){
             rotation = heading + 180;
           }
 
-          function setTrainIconSize(zoomLevel) {
-            var sizesToMatchZooms = {
-              12: 1,
-              13: 2,
-              14: 4,
-              15: 6,
-              16: 8
-            };
-           return sizesToMatchZooms[zoomLevel];
-          };
           scaleSizeByZoomLevel = setTrainIconSize(map.getZoom())
-
           var customImage = {
             path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
             scale: scaleSizeByZoomLevel,
